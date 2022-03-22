57d7eba5f17aa9af1c03a01a4cf059b38fd31b92
uhhernadez <uh.hernandez@ugto.mx>
2022-03-22 12:44:14 -0600

    D3 introducción

--- 05_viz_web/html/04_varios_archivos/app.jsx
+++ 05_viz_web/html/04_varios_archivos/app.jsx
@@ -2,6 +2,9 @@ const width = 500;
 const height = 500;
 const nombre = 'Juan';
 console.log('Esto es un mensaje');
+
+console.log('😎');
+
 ReactDOM.render(
   <svg width={width} height={height} > 
   <circle cx={width/2.0} cy={height/2.0} r="30"> </circle>
---
+++ 07_react_d3/00_ejemplo/app.jsx
@@ -0,0 +1,10 @@
+const width = 500;
+const height = 500;
+const nombre = 'Juan';
+
+ReactDOM.render(
+  <svg width={width} height={height} >
+      <circle cx={width/2.0} cy={height/2.0} r="30"> </circle>
+  </svg>,
+  document.getElementById('root')
+);
