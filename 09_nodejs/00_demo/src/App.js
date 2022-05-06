ae2d5cc2cebbe7152614f4557c2c76a720771ac0
uhhernadez <uh.hernandez@ugto.mx>
2022-05-06 15:08:11 -0500

    Demo aplicación

---
+++ 09_nodejs/00_demo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+const width = 500;
+const height = 500;
+
+function App() {
+  return (
+    <svg width={width} height={height} >
+    </svg>
+  );
+}
+
+export default App;
--- 09_nodejs/01_base/src/App.js
+++ 09_nodejs/01_base/src/App.js
@@ -1,17 +1,16 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { csv } from 'd3';
+import React, { useState, useEffect } from 'react';
+import * as d3 from 'd3';
 
 const width = 500;
 const height = 500;
 
-const csvUrl = 'https://raw.githubusercontent.com/uhhernadez/procesamientoDeDatos2022A/main/08_react_sql_d3/01_componente/db/Casos_Diarios_Municipio_Confirmados_20220407.csv';
+const csvUrl = 'db/Casos_Diarios_Municipio_Confirmados_20220407.csv';
 
 const Datos = () => {
   const [datos, actualizaDatos ] = useState(null); 
   useEffect(() => {
     console.log('🦧🤓');
-    csv(csvUrl).then(data => {
+    d3.csv(csvUrl).then(data => {
       actualizaDatos(data[500]);
       
       console.log(data[500]);
